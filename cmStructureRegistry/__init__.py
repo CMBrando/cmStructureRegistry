e60455e84f5982124674963af57acc2636f712f7
Brandon Kuehl <cmbrando999@gmail.com>
2025-10-26 18:57:07 -0500

    Version bump due to period task changes, pulling versioning from __version__ as true source.

--- cmStructureRegistry/__init__.py
+++ cmStructureRegistry/__init__.py
@@ -1,6 +1,6 @@
-"""Example plugin app for Alliance Auth."""
+"""Structure Registry app for Alliance Auth."""
 
 # pylint: disable = invalid-name
 default_app_config = "cmStructureRegistry.apps.DefaultConfig"
 
-__version__ = "0.1.0"
+__version__ = "0.995"
--- cmStructureRegistry/app_settings.py
+++ cmStructureRegistry/app_settings.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings
 
-CM_VERSION = "0.991"
+from cmStructureRegistry import __version__ as CM_VERSION
 
 # Uncomment line below to specify corporations ids to limit to for Friendly timers (notification_timer_task)#
 #FRIENDLY_TIMER_CORP_IDS = []
