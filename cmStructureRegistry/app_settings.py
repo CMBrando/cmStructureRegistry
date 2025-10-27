8f131c01b94650c46edd39c10fe679b3d6fc00c0
Denny <909666+ohhidenny@users.noreply.github.com>
2025-10-27 11:20:12 +1030

    settings - use getattr

--- cmStructureRegistry/app_settings.py
+++ cmStructureRegistry/app_settings.py
@@ -4,5 +4,4 @@ from django.conf import settings
 
 from cmStructureRegistry import __version__ as CM_VERSION
 
-# Uncomment line below to specify corporations ids to limit to for Friendly timers (notification_timer_task)#
-#FRIENDLY_TIMER_CORP_IDS = []
+FRIENDLY_TIMER_CORP_IDS = getattr(settings, "FRIENDLY_TIMER_CORP_IDS", [])
